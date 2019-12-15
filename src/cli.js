a9f223193a02362e13337a58f048a56b9fd9e020
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2019-12-15 17:49:58 +0100

    added cli initial commit

---
+++ src/cli.js
@@ -0,0 +1,24 @@
+import arg from "arg";
+
+const parseArgsIntoOptions = rawArgs => {
+  const args = arg(
+    {
+      "--yes": Boolean,
+      "-y": "--yes"
+    },
+    {
+      argv: rawArgs.slice(2)
+    }
+  );
+
+  // Q - ready to create your awesome package?
+
+  return {
+    skipPrompts: args["--yes"] || false,
+    template: args._[0]
+  };
+};
+
+export async function cli(args) {
+  const options = parseArgsIntoOptions(args);
+}
