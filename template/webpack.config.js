272b39e43f367f3a8d1bde21f46637d99c75404c
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2019-12-15 18:43:01 +0100

    webpack config for the template

---
+++ template/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
