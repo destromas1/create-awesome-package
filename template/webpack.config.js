34908156decc46d4e3e6c5255c56487c4b5609a7
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2019-12-26 17:27:28 +0100

    fixed webpack source path

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js"
