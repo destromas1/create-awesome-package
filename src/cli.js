b7ce926fc806b84aebab20c68cfe44ca8cd620da
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2020-01-03 23:56:51 +0100

    get the template from args

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
 import arg from "arg";
+import chalk from "chalk";
 import { createAwesomePackage } from "./main";
+import { templates } from "./constants";
 
 const parseArgsIntoOptions = rawArgs => {
   const args = arg(
@@ -14,10 +16,18 @@ const parseArgsIntoOptions = rawArgs => {
 
   // Q - ready to create your awesome package?
 
+  const packageName = args._[0];
+  const template = args._[1];
+
+  if (!templates.includes(template)) {
+    console.error(chalk.red.bold("Template should be either 'js' or 'ts'"));
+    process.exit(1);
+  }
+
   return {
     skipPrompts: args["--yes"] || false,
-    packageName: args._[0],
-    // template: args._[0]
+    packageName,
+    template
   };
 };
 
