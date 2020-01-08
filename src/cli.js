144f93c395a5ade7e38f26e3bed5e74040309b14
Shahjada Talukdar <shahjada.talukdar@gmail.com>
2020-01-08 21:23:12 +0100

    prompt for template

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import arg from "arg";
 import chalk from "chalk";
+import inquirer from "inquirer";
 import { createAwesomePackage } from "./main";
-import { templates } from "./constants";
+import { TemplateEnum, TemplateMapper } from "./constants";
 
 const parseArgsIntoOptions = rawArgs => {
   const args = arg(
@@ -17,22 +18,44 @@ const parseArgsIntoOptions = rawArgs => {
   // Q - ready to create your awesome package?
 
   const packageName = args._[0];
-  const template = args._[1];
+  // const template = args._[1];
 
-  if (!templates.includes(template)) {
-    console.error(chalk.red.bold("Template should be either 'js' or 'ts'"));
-    process.exit(1);
-  }
+  // if (!templates.includes(template)) {
+  //   console.error(chalk.red.bold("Template should be either 'js' or 'ts'"));
+  //   process.exit(1);
+  // }
 
   return {
     skipPrompts: args["--yes"] || false,
-    packageName,
-    template
+    packageName
+    // template
   };
 };
 
+async function promptForTemplate(params) {
+  const answers = await inquirer.prompt([
+    {
+      type: "list",
+      name: "template",
+      message: "Please select your Template language",
+      choices: [TemplateEnum.JavaScript, TemplateEnum.TypeScript]
+    }
+  ]);
+
+  const mappedTemplate = TemplateMapper[answers.template];
+
+  return mappedTemplate;
+}
+
 export async function cli(args) {
-  const options = parseArgsIntoOptions(args);
+  let options = parseArgsIntoOptions(args);
+
+  const template = await promptForTemplate(options);
+
+  options = {
+    ...options,
+    template
+  };
 
   await createAwesomePackage(options);
 }
